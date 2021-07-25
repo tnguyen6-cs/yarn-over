657aa9c6b1a4371ea497374c83b5ab253f185e16
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2021-07-25 17:04:56 -0400

    Progress

--- webscrape.py
+++ webscrape.py
@@ -1,14 +1,19 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import sqlite3
 
 my_url = 'https://www.amigurumi.com/search/free/'
+#Connect to sqlite3 database
+conn = sqlite3.connect('my_database.db')
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS my_table(pattern TEXT,link TEXT,description TEXT,category TEXT)''')
 # Open csv file
-filename = "patterns.csv"
-f = open(filename, "w")
-headers = "pattern, link, description, category\n"
-f.write(headers)
-for page in range (1,10): #parse 10 pages
+#filename = "patterns.csv"
+#f = open(filename, "w")
+#headers = "pattern, link, description, category\n"
+#f.write(headers)
+for page in range (1,2): #parse 10 pages
     new_url = my_url + str(page) + '/'
     uClient = uReq(new_url)
     page_html = uClient.read()
@@ -43,6 +48,12 @@ for page in range (1,10): #parse 10 pages
         
         group = category[1].text
 
-        f.write(name + "," +  link + "," + new_des_text.replace(",", " ") + ","  + group + "\n")
-
-f.close()
+        #f.write(name + "," +  link + "," + new_des_text.replace(",", " ") + ","  + group + "\n")
+        cur.execute('''INSERT INTO my_table(pattern,link, description, category) VALUES(?,?,?,?)''', (name, link, new_des_text, group))
+        conn.commit()
+#f.close()
+#cur.execute('''SELECT * FROM my_table ''')
+#results= cur.fetchall()
+#print(results)
+cur.close()
+conn.close()
